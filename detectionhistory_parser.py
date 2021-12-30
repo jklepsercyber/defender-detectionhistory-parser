117a8e37ca1c8b1c936d65d99d8a9be40c56a5cf
Jordan Klepser <94950521+jklepsercyber@users.noreply.github.com>
2021-12-29 23:10:30 -0600

    wip - handle guid correctly
    wip - handle guid correctly

--- detectionhistory_parser.py
+++ detectionhistory_parser.py
@@ -18,26 +18,47 @@ def parse_header_and_guid(file):
     guid_oct.append(binascii.hexlify(file.read(6))) # this hex is not flipped in file data
     
     # HANDLE GUID HERE
-    
-    return guid_oct[0]
+    oct_count = 0
+    while oct_count<=2:
+        oct = guid_oct[oct_count]
+        print(oct)
+        newlist = list(binascii.hexlify(oct))
+        newlist = [oct[i:i+2] for i in range(0, len(oct), 2)]
+        newlist.reverse()
+        guid_oct[oct_count] = b''.join(newlist)
+        oct_count=oct_count+1
+
+    guid_final = (guid_oct[0].decode('utf-8')+"-"
+        +guid_oct[1].decode('utf-8')+"-"
+        +guid_oct[2].decode('utf-8')+"-"
+        +guid_oct[3].decode('utf-8')+"-"
+        +guid_oct[4].decode('utf-8'))
+
+    return guid_final
+
 
 def parse_detection_history(my_args):
 
     filepath = my_args["filepath"]
+    parsed_value_dict = dict()
+
+    # DEFINE MODES
     KEY_READ_MODE = b'\x00'
     VALUE_READ_MODE = b'\x01'
     NULL_DATA_MODE = b'\x02'
-    MAGIC_VERSION_SECTION = b'\x01'
-    CURRENT_STATE = KEY_READ_MODE
-    parsed_value_dict = dict()
+    CURRENT_MODE = KEY_READ_MODE
+    # DEFINE SECTIONS
+    MAGIC_VERSION_SECTION = b'\x04'
+    GENERAL_SECTION = b'\x05'
 
     with open(filepath, 'rb') as f:
         parsed_value_dict["GUID"] = parse_header_and_guid(f)
         f.read(8) # skipping over some empty space
+
         while True:
             temp_key = ""
             while MAGIC_VERSION_SECTION:
-                if CURRENT_STATE==KEY_READ_MODE: 
+                if CURRENT_MODE==KEY_READ_MODE: 
                     print("Key read mode")
                     chunk = f.read(2)
                     if chunk==b'\x3A\x00': # first few sections are delimited by a Windows-1252 colon rather than multiple \x00 bytes
@@ -45,7 +66,7 @@ def parse_detection_history(my_args):
                         temp_key = re.sub("\x00", "", temp_key)
                         parsed_value_dict[temp_key] = "" # we will reset temp_key after setting the value
                         print(parsed_value_dict)
-                        CURRENT_STATE = VALUE_READ_MODE 
+                        CURRENT_MODE = VALUE_READ_MODE 
                     else:
                         temp_key = temp_key+chunk.decode('windows-1252')
                         print(chunk)
@@ -56,7 +77,7 @@ def parse_detection_history(my_args):
                             temp_key = re.sub("\x00", "", temp_key)
                             parsed_value_dict[temp_key] = "" # make sure "file" key gets assigned
                             MAGIC_VERSION_SECTION = 0 # break out of this section
-                elif CURRENT_STATE==VALUE_READ_MODE:
+                elif CURRENT_MODE==VALUE_READ_MODE:
                     print("Value read mode")
                     chunk = f.read(2)
                     if chunk==b'\x00\x00': # if chunk to be read is empty
@@ -65,20 +86,22 @@ def parse_detection_history(my_args):
                             print("Switching to Null Data Mode")
                             parsed_value_dict[temp_key] = re.sub("\x00", "", parsed_value_dict[temp_key]) # finalize value
                             temp_key = "" # reset temp key for next run
-                            CURRENT_STATE=NULL_DATA_MODE
+                            CURRENT_MODE=NULL_DATA_MODE
                     else:
                         parsed_value_dict[temp_key] = parsed_value_dict[temp_key] + str(chunk.decode('windows-1252'))
                         print(chunk)
                         print(parsed_value_dict[temp_key])
-                elif CURRENT_STATE==NULL_DATA_MODE:
+                elif CURRENT_MODE==NULL_DATA_MODE:
                     print("Null data mode")
                     chunk = f.read(2)
                     if len(re.sub(r'\W+', '', chunk.decode('windows-1252')))>=1: # regex function removes all non-alphanum characters
                         print(chunk.decode('windows-1252'))
                         temp_key = temp_key+chunk.decode('windows-1252')
                         print("Switching to Key Read Mode")
-                        CURRENT_STATE = KEY_READ_MODE
-            
+                        CURRENT_MODE = KEY_READ_MODE
+            while GENERAL_SECTION:
+                GENERAL_SECTION = 0
+                pass
             chunk = f.read(2)
             if not chunk:
                 print("EOF detected. Closing...")
